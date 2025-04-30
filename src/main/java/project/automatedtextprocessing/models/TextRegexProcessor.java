30f66038470dbe370ad8efd81017267d59914e80
Kaytee <kofi.taylor07@gmail.com>
2025-04-30 11:11:47 +0000

    feat: add TextRegexProcessor class to find matches and replaces matches for both regex abd text

--- src/main/java/project/automatedtextprocessing/models/TextRegexProcessor.java
+++ src/main/java/project/automatedtextprocessing/models/TextRegexProcessor.java
@@ -1,11 +1,16 @@
 package project.automatedtextprocessing.models;
 
+import project.automatedtextprocessing.Utils.RegexMatchResult;
+
 import java.util.ArrayList;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
 public class TextRegexProcessor {
     private Pattern patternObj;
+    ArrayList<RegexMatchResult> results;
+    int matchedWordIndex = 0;
 
     public void  compileRegexPattern(String pattern){
         try {
@@ -20,7 +25,57 @@ public class TextRegexProcessor {
     }
 
     public ArrayList<RegexMatchResult> getAllMatches(String Input){
-
+        if (patternObj == null) {
+            throw new IllegalStateException("Regex pattern is null: call compileTextPattern or compileRegexPattern");
+        }
+        results = new ArrayList<>();
+        matchedWordIndex = 0;
+        Matcher matcher = patternObj.matcher(Input);
+        while (matcher.find()){
+            results.add(new RegexMatchResult(matcher.group(), matcher.start(), matcher.end()));
+        }
+        return results;
     }
+
+    public RegexMatchResult getMatchResult(){
+        if (results == null){
+            return null;
+        }
+        return results.get(matchedWordIndex);
+    }
+
+    public void nextMatchIndex(){
+        if (matchedWordIndex < results.size()){
+            matchedWordIndex++;
+        }
+    }
+
+    public  void prevMatchIndex(){
+        if (matchedWordIndex > 0){
+            matchedWordIndex--;
+        }
+    }
+
+
+    public String replaceAllMatches(String input, String replacement){
+        if (patternObj == null) {
+            throw new IllegalStateException("Regex pattern is null: call compileTextPattern or compileRegexPattern");
+        }
+        return patternObj.matcher(input).replaceAll(replacement);
+    }
+
+    public String replaceAMatch(String input, String replacement){
+        if (results == null || results.isEmpty()){
+            return input;
+        }
+        RegexMatchResult matchResult = getMatchResult();
+        StringBuilder result = new StringBuilder();
+        result.append(input.substring(0, matchResult.getStartIndex()));
+        result.append(replacement);
+        result.append(input.substring(matchResult.getEndIndex()));
+
+        return  result.toString();
+    }
+
 
 }
